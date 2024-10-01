c7ca6d1e5f460049ae323ed387e21df7cd60d905
Anselmo Parente Martins <anselmo.martins@unifametro.edu.br>
2024-10-01 14:38:52 -0300

    feat: Set preferred orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MainApp());
+  });
 }
 
 class MainApp extends StatelessWidget {
